0b9a718f3d6f0dc3903e7785e258165c65530b9b
Chris Junker <elevatemedfit@gmail.com>
2019-11-04 13:59:48 +0000

    Automatically backed up by Learn

--- app/models/add_foriegn_keys_to_ride.rb
+++ app/models/add_foriegn_keys_to_ride.rb
@@ -1,6 +1,6 @@
 class AddForiegnKeysToRide < ApplicationRecord
-  def change
-    add_column :rides, :passenger_id, :integer
-    add_column :rides, :taxi_id, :integer
-  end
+  # def change
+  #   add_column :rides, :passenger_id, :integer
+  #   add_column :rides, :taxi_id, :integer
+  # end
 end
